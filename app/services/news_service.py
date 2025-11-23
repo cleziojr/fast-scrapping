a668673593e68e7a033766b334eabe00cf18f798
clezio <cleziojr9@gmail.com>
2025-11-23 12:03:10 -0300

    :broom: cleanup: simplificando o projeto

---
+++ app/services/news_service.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get("https://www.cnnbrasil.com.br/")
+soup = BeautifulSoup(r.text, "html.parser")
+
+# Seleciona todos os figcaption com as classes desejadas
+item_principal = soup.select("figcaption.flex.w-full.flex-col.items-stretch.gap-2.text-wrap")[0].find("h2")
+print("Título:", item_principal.get_text(strip=True))
